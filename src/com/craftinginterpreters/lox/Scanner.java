994f6912727765fe8948f0b18e01f06dcb79422a
Leo Rouleau <Leorouleau5070@gmail.com>
2025-12-23 22:29:26 -0500

    string and number literals

--- src/com/craftinginterpreters/lox/Scanner.java
+++ src/com/craftinginterpreters/lox/Scanner.java
@@ -13,6 +13,9 @@ class Scanner {
     private int start = 0;
     private int current = 0;
     private int line = 1;
+
+    
+
     
     Scanner(String source) {
 	this.source = source;
@@ -71,16 +74,62 @@ class Scanner {
 	case '\n':
 	    line++;
 	    break;
+
+	case '"': string(); break;
 	    
 	default:
-	    Lox.error(line,"Unexpected character.");
-	    break;
+	    if (isDigit(c)) {
+		number();
+	    } else if (isAlpha(c)){
+		identifier();
+	    } else {
+		Lox.error(line,"Unexpected character.");
+		break;
+	    }
 	}
+    }
+
+    private void identifier() {
+	while (isAlphaNumeric(peek())) advance();
+
+	addToken(IDENTIFIER);
+    }
+
+    private void number() {
+	while (isDigit(peek())) advance();
+
+	if (peek() == '.' && isDigit(peekNext())) {
+	    advance();
+
+	    while (isDigit(peek())) advance();
+	}
+
+	addToken(NUMBER,Double.parseDouble(source.substring(start,current)));
+	
+    }
+
+    private void string() {
+	while (peek() != '"' && !isAtEnd()) {
+	    if (peek() == '\n') line++;
+	    advance();
+	}
+
+	if (isAtEnd()) {
+	    Lox.error(line,"Unterminated string.");
+	    return;
+	}
+
+	// Closing 
+	advance();
+
+	// Trim
+	String value = source.substring(start + 1, current -1);
+	addToken(STRING,value);
     }
 
     private boolean match(char expected) {
 	if(isAtEnd()) return false;
-	if(source.CharAt(current) != expected) return false;
+	if(source.charAt(current) != expected) return false;
 
 	current++;
 	return true;
@@ -88,7 +137,24 @@ class Scanner {
 
     private char peek() {
 	if(isAtEnd()) return '\0';
-	return source.CharAt(current);
+	return source.charAt(current);
+    }
+
+    private char peekNext() {
+	if (current + 1 >= source.length()) return '\0';
+	return source.charAt(current + 1);
+    }
+
+    private boolean isAlpha(char c) {
+	return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '_';
+    }
+
+    private boolean isAlphaNumeric(char c) {
+	return isAlpha(c) || isDigit(c);
+    }
+    
+    private boolean isDigit(char c) {
+	return c >= '0' && c <= '9';
     }
     
     private boolean isAtEnd() {
@@ -96,7 +162,7 @@ class Scanner {
     }
 
     private char advance() {
-	return source.CharAt(current++);
+	return source.charAt(current++);
     }
 
     private void addToken(TokenType type) {
