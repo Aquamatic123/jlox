6b71420e11302767304e23e8987a059a8c88f0e2
Leo Rouleau <Leorouleau5070@gmail.com>
2025-12-26 23:08:45 -0500

    RuntimeError file creation

--- src/com/craftinginterpreters/lox/Interpreter.java
+++ src/com/craftinginterpreters/lox/Interpreter.java
@@ -15,6 +15,7 @@ class Interpreter implements Expr.Visitor<Object> {
 	case BANG:
 	    return !isTruthy(right);
 	case MINUS:
+	    checkNumberOperand(expr.operator, right);
 	    return -(double)right;
 	}
 
@@ -22,11 +23,22 @@ class Interpreter implements Expr.Visitor<Object> {
 	return null;
     }
 
+    private boolean checkNumberOperand(Token operator, Object operand) {
+	if(operand instanceof Double) return;
+	throw new RuntimeError(operator, "Operand must be a number.");
+    }
+
     private boolean isTruthy(Object object) {
 	if (object == null) return false;
 	if (object instanceof Boolean) return (boolean)object;
 	return true;
-			 
+    }
+
+    private boolean isEqual(Object a, Object b) {
+	if (a == null && b == null) return true;
+	if (a == null) return false;
+
+	return a.equals(b);
     }
 
     @Override
@@ -36,6 +48,43 @@ class Interpreter implements Expr.Visitor<Object> {
 
     private Object evaluate(Expr expr) {
 	return expr.accept(this);
+    }
+
+    @Override
+    public Object visitBinaryExpr(Expr.Binary expr) {
+	Object left = evaluate(expr.left);
+	Object right = evaluate(expr.right);
+
+	switch (expr.operator.type) {
+	case GREATER:
+	    return (double)left > (double)right;
+	case GREATER_EQUAL:
+	    return (double)left >= (double)right;
+	case LESS:
+	    return (double)left < (double)right;
+	case LESS_EQUAL:
+	    return (double)left <= (double)right;
+	case MINUS:
+	    return (double)left - (double)right;
+	case PLUS:
+	    if (left instanceof Double && right instanceof Double) {
+		return (double)left + (double)right;
+	    }
+
+	    if (left instanceof String && right instanceof String) {
+		return (String)left + (String)right;
+	    }
+
+	    break;
+	case SLASH:
+	    return (double)left / (double)right;
+	case STAR:
+	    return (double)left * (double)right;
+	case BANG_EQUAL:
+	    return !isEqual(left, right);
+	case EQUAL_EQUAL:
+	    return isEqual(left, right);
+	}
     }
 
 
