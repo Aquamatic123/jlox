fc0576a33836b11247b5fa4a4ba41fe7042a8947
Leo Rouleau <Leorouleau5070@gmail.com>
2025-12-30 15:19:10 -0500

    functions (chapter 10) before LoxCallable

--- src/com/craftinginterpreters/lox/Interpreter.java
+++ src/com/craftinginterpreters/lox/Interpreter.java
@@ -1,5 +1,6 @@
 package com.craftinginterpreters.lox;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
@@ -221,5 +222,18 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 	return null;
     }
 
+    @Override
+    public Object visitCallExpr(Expr.Call expr) {
+	Object callee = evaluate(expr.callee);
 
+	List<Object> arguments = new ArrayList<>();
+	for (Expr argument : expr.arguments) {
+	    arguments.add(evaluate(argument));
+	}
+
+    LoxCallable function = (LoxCallable)callee;
+    return function.call(this, arguments);
+    }
 }
+
+
